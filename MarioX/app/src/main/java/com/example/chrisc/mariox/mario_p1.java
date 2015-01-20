d069ec2bbe52a046bdc36fbcc6be733d144ceb99
Chris Chew <chchew1110@gmail.com>
2015-01-19 18:52:43 -0800

    Added the Queue class, and the Mario class

---
+++ MarioX/app/src/main/java/com/example/chrisc/mariox/mario_p1.java
@@ -0,0 +1,11 @@
+package com.example.chrisc.mariox;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class mario_p1 extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mario_p1);
+    }
+}
